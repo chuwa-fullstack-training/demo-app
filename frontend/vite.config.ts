f66c7f24b4625ca2c4a8b765b3a0c0bcccaa945c
Aaron Zhang <react.training@chuwaamerica.com>
2024-09-12 23:54:57 -0700

    set up client

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,5 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,5 +9,10 @@ export default defineConfig({
   server: {
     host: true,
     port: 3001
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(path.dirname(fileURLToPath(import.meta.url)), './src')
+    }
   }
 });
