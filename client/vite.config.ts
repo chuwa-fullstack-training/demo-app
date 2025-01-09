de0ab8ecff43cc639d092f3fafef00c04535975d
Aaron Zhang <react.training@chuwaamerica.com>
2025-01-08 16:36:22 -0800

    update

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -2,30 +2,19 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'node:path';
 import { fileURLToPath } from 'node:url';
-import { nodePolyfills } from 'vite-plugin-node-polyfills';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), nodePolyfills()],
+  plugins: [react()],
   server: {
     host: true,
-    port: 3001,
+    port: 4001,
     proxy: {
       '/api': {
         target: 'http://localhost:3000',
         changeOrigin: true,
         secure: false,
-        ws: true,
-      },
-    },
-  },
-  preview: {
-    proxy: {
-      '/api': {
-        target: 'https://demo-app-h712.onrender.com',
-        changeOrigin: true,
-        secure: false,
-        ws: true,
+        // ws: true,
       },
     },
   },
